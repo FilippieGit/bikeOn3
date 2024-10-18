3cad6dd94d3e7b078d936eba2d141aec0d5f0161
FilippieGit <filippieamaral@gmail.com>
2024-10-17 21:08:08 -0300

    v2

--- app/src/main/java/br/com/etec/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etec/myapp/BikesNovasFragment.java
@@ -3,13 +3,20 @@ package br.com.etec.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BikesNovasFragment extends Fragment {
+
+    private List<BikeNova> lstBikeNova;
+    RecyclerView idRecBikesNovas;
 
 
     @Override
@@ -18,6 +25,17 @@ public class BikesNovasFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+
+        idRecBikesNovas = view.findViewById(R.id.idRecBikesNovas);
+
+        //carregar objetos da lista
+        lstBikeNova = new ArrayList<>();
+        lstBikeNova.add(
+                new BikeNova("Vintage",R.drawable.bike_um);
+        );
+
+
+
 
         return view;
     }
